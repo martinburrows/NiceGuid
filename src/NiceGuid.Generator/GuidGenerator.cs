1430f507814e7cd9a910bed016f508d2b23c1c86
martin <martinjrb@gmail.com>
2015-03-23 00:31:51 +0000

    added project

---
+++ src/NiceGuid.Generator/GuidGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NiceGuid.Generator
+{
+    public class GuidGenerator
+    {
+        private readonly ConcurrentDictionary<int, List<string>> _wordsByLength;
+        private readonly Random _random;
+
+        public GuidGenerator(ConcurrentDictionary<int, List<string>> wordsByLength)
+        {
+            _wordsByLength = wordsByLength;
+            _random = new Random();
+        }
+
+        public string GetNiceGuid()
+        {
+            var segments = new[]
+                           {
+                               GetSegment(8),
+                               GetSegment(4),
+                               GetSegment(4),
+                               GetSegment(4),
+                               GetSegment(12)
+                           };
+
+            return string.Join("-", segments);
+        }
+
+        string GetSegment(int segmentLength)
+        {
+            var wordLengths = GetSegmentWordLengths(segmentLength);
+
+            return wordLengths.Aggregate(string.Empty, (current, length) => current + GetRandomWord(length));
+        }
+
+        private IEnumerable<int> GetSegmentWordLengths(int totalLength)
+        {
+            var remainingLength = totalLength;
+
+            while (remainingLength > 0)
+            {
+                var possibleLengths = GetRemainingPossibleWordLengths(remainingLength).ToList();
+                var selected = possibleLengths.Count == 1 ? possibleLengths[0] : possibleLengths[_random.Next(possibleLengths.Count)];
+
+                yield return selected;
+
+                remainingLength -= selected;
+            }
+        }
+
+        public IEnumerable<int> GetRemainingPossibleWordLengths(int remainingLength)
+        {
+            const int minWordLength = 4;
+            const int maxWordLength = 8;
+
+            if (remainingLength <= maxWordLength)
+                yield return remainingLength;
+
+            if (remainingLength == minWordLength || remainingLength < (minWordLength*2))
+                yield break;
+
+            yield return minWordLength;
+
+            var maxVariable = remainingLength - (minWordLength*2);
+
+            if (maxVariable <= 0)
+                yield break;
+
+            foreach (var i in Enumerable.Range(minWordLength + 1, maxVariable))
+                yield return i;
+        }
+
+        string GetRandomWord(int length)
+        {
+            return _wordsByLength[length][_random.Next(_wordsByLength[length].Count)];
+        }
+    }
+}
---
+++ src/NiceGuid.Web/Controllers/GuidController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using NiceGuid.Generator;
+
+namespace NiceGuid.Web.Controllers
+{
+    public class GuidController : Controller
+    {
+        private readonly IGuidGeneratorFactory _guidGeneratorFactory;
+
+        public GuidController(IGuidGeneratorFactory guidGeneratorFactory)
+        {
+            _guidGeneratorFactory = guidGeneratorFactory;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Guid = GetGuid();
+            return View();
+        }
+
+        public ActionResult Generate()
+        {
+            if (!Request.IsAjaxRequest()) return HttpNotFound();
+            return Content(GetGuid());
+        }
+
+        private string GetGuid()
+        {
+            return _guidGeneratorFactory.GetGuidGenerator().GetNiceGuid();
+        }
+    }
+}
